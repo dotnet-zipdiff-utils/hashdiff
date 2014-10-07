5ac075aaf71639eb4ec96c27da21cf35028331d0
leekelleher <leekelleher@gmail.com>
2014-10-07 16:11:07 +0100

    Added HashDiff class library and console app.

---
+++ src/HashDiff.Core/Output/AbstractBuilder.cs
@@ -0,0 +1,17 @@
+﻿namespace HashDiff.Core.Output
+{
+	using System.IO;
+
+	public abstract class AbstractBuilder : IBuilder
+	{
+		public void Build(string path, Differences diff)
+		{
+			using (var writer = new StreamWriter(path))
+			{
+				Build(writer, diff);
+			}
+		}
+
+		public abstract void Build(StreamWriter writer, Differences diff);
+	}
+}
---
+++ src/HashDiff.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HashDiff.Core")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("355B88F9-1774-4CD1-B3DA-C96CF9C7AAC6")]

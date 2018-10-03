49f5c9ea8ca34e8b158d949bfef4d738d852b74c
leekelleher <leekelleher@gmail.com>
2018-10-03 10:41:44 +0100

    Bumped version number to v1.1.1-alpha for any hotfixes

--- src/VersionInfo.cs
+++ src/VersionInfo.cs
@@ -9,6 +9,6 @@
 //------------------------------------------------------------------------------
 
 [assembly: System.Reflection.AssemblyVersion("1.1.*")]
-[assembly: System.Reflection.AssemblyInformationalVersion("1.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.1.1-alpha")]
 
 
